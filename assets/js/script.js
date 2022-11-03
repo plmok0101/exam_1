5119f3aae68f1ac8006638dd9de79091d2bd7336
plmok0101 <plmokn1q2w3e@gmail.com>
2022-11-03 15:31:50 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+function aaa(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    if(document.form.email.value.length==0){
+        alert('이메일을 입력해주세요.');
+        form.email.focus();
+        return false;
+    }
+    if(!CheckEmail(form.email.value)){
+        alert("이메일 형석이 올바르지 않습니다.");
+        form.email.focus();
+        return false;
+    }
+    if(document.form.password.value.length==0){
+        alert('비밀번호를 입력해주세요');
+        form.password.focus();
+        return false;
+    }
+    alert("로그인되었습니다.")
+    return false;
+}
+function CheckEmail(str){                                                 
+    var reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/i;
+    if(!reg_email.test(str)) {                            
+       return false;         
+    }else {                       
+       return true;         
+    }                            
+}
