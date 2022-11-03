1aab1b7d1c55acab9d0591422b0768a4b6656d38
plmok0101 <plmokn1q2w3e@gmail.com>
2022-11-03 17:58:39 +0900

    js import

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-document.write("<script src='asset/js/jquery.js'></script>");
+document.write("<script src='jquery.js'></script>");
 function aaa(){
     var email = document.getElementById("email");
     var password = document.getElementById("password");
