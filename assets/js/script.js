435b13560620e63e04f4c74240aa5a9c96d75d3a
plmok0101 <plmokn1q2w3e@gmail.com>
2022-11-04 17:20:23 +0900

    12313

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,31 @@
-/*function aaa(){
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-    if(document.form.email.value.length==0){
-        alert('이메일을 입력해주세요.');
-        form.email.focus();
-        return false;
-    }
-    if(!CheckEmail(form.email.value)){
-        alert("이메일 형석이 올바르지 않습니다.");
-        form.email.focus();
-        return false;
-    }
-    if(document.form.password.value.length==0){
-        alert('비밀번호를 입력해주세요');
-        form.password.focus();
-        return false;
-    }
-    alert("로그인되었습니다.")
-    return false;
+function check(){
+    var check_email = RegExp(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/);
+    if($("#email").val() == ""){
+            alert("이메일을 입력해주세요.");
+            $("#email").focus();
+            return false;
+        }
+        if(!check_email.test($("#email").val())){
+            alert("이메일형식에 맞게 입력해주세요");
+            $("#email").focus();
+            return false;
+        }
+        
+        if($("#password").val() == ""){
+            alert("비밀번호를 입력해주세요.");
+            $("#password").focus();
+            return false;
+        }
 }
-function CheckEmail(str){                                                 
-    var reg_email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/i;
-    if(!reg_email.test(str)) {                            
-       return false;         
-    }else {                       
-       return true;         
-    }                            
-}*/
+
+$(document).ready(function(){
+    alert("reset");
+    $(".popupButton").click(function(){
+        if($(this).hasClass("facebook")){
+            window.open("https://ko-kr.facebook.com/","fecebookPopup","width=800, height=800");
+        }
+        if($(this).hasClass("google")){
+            window.open("https://www.google.co.kr/","GooglePopup","width=800, height=800");
+        }
+    })
+})
